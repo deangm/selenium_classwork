5710838bad6bbd501411e26f077c9801a09a77e2
mikey <michael.d.deangelo@Gmail.com>
2020-05-11 18:09:56 -0600

    classwork selenium

---
+++ exercise_5/test.js
@@ -0,0 +1,25 @@
+var webdriver = require('selenium-webdriver'),
+	By = webdriver.By,
+	until = webdriver.until;
+
+var driver = new webdriver.Builder()
+	.forBrowser('chrome')
+	.build();
+
+driver.get('https://loghen41.github.io');
+
+// Good job manually verifying the page has been updated, I wonder if there is a way we can automatically check that rather than making the page wait.
+// Try finding a way to verify the inputs value, and have the system run the following command if it finds the right text: console.log("text found");
+
+input = driver.findElement(By.className('fullInput'))
+input.sendKeys('Write some Code');
+// driver.sleep(2000);
+value = input.getAttribute("value")
+
+value.then(thing => {
+	if (thing == 'Write some Code'){
+		console.log(thing)
+	}
+})
+
+driver.quit();
